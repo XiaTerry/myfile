9c8953ec890b8bce53b02aaca0b34379ceb486d3
XiaTerry <1363314380@qq.com>
2018-08-13 23:41:54 +0800

    vue 实例

---
+++ vue-cli/vueapp/src/components/CommonFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul class="footer" :style="{background:bgColor}">
+        
+        <li class="footer-menu" v-for="(menu,index) in menuList" :key="index">
+            
+            <router-link :to="menu.path" @click.native="changeTitle(menu)">
+                {{menu.title}}
+            </router-link>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+   props:["menuList"],
+   data () {
+       return {
+            bgColor:"",      
+       }
+   },
+   methods:{
+       changeTitle(menu){
+           this.bgColor = menu.bgColor;
+           this.$emit('changeTitle',menu);
+           console.log(menu.icon);
+       }
+   }
+}
+
+</script>
+<style lang="scss" scoped>
+    .footer{
+        height: 1rem;
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+        display: flex;
+        background: #f00;
+        color: #fff;
+        &-menu{
+            flex-grow: 1;
+            text-align: center;
+            line-height: 1rem;
+        }
+        &-menu a{
+            color: #fff;
+            outline: none;
+            font-size: 18px;
+        }
+    }
+</style>
---
+++ vue-cli/vueapp/src/components/CommonHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="header" :style="{background:selectMenu.bgColor}">
+        <span class="header-home">首页</span>
+        <h3 class="header-title">{{selectMenu.title?selectMenu.title:"电影"}}</h3>
+    </header>
+</template>
+<script>
+export default {
+    props:["selectMenu"],
+}
+</script>
+<style lang="scss" scoped>
+    .header{
+        height: 1rem;
+        background: #f00;
+        width: 100%;
+        position: fixed;
+        line-height: 1rem;
+        top: 0;
+        left: 0;
+        &-title{
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%);
+            font-weight: bold;
+        }
+        &-home{
+            margin-left: 0.2rem;
+            font-size: 18px;
+        }
+    }
+</style>
