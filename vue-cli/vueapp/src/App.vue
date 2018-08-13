627361ad9ec8413a9f79c09ec8a39d950b0ee0c9
XiaTerry <1363314380@qq.com>
2018-08-13 23:41:03 +0800

    vue实列

---
+++ vue-cli/vueapp/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <CommonHeader :selectMenu="selectMenu"></CommonHeader>
+    <router-view class="content"/>
+    <CommonFooter :menuList="menuList" @changeTitle="changeTitle">
+    </CommonFooter>
+  </div>
+</template>
+<script>
+import CommonHeader from '@/components/CommonHeader.vue';
+import CommonFooter from '@/components/CommonFooter.vue';
+export default {
+  components:{
+    CommonHeader,
+    CommonFooter,
+  },
+  methods:{
+    changeTitle(menu){
+      this.selectMenu = menu;
+      
+    }
+  },
+  data(){
+    return{
+      menuList:[
+      {
+        title:'电影',
+        name:'movie',
+        path:'/movie',
+        bgColor:'#f00',
+        icon:'icon-dianying'
+      },
+      {
+        title:'音乐',
+        name:'music',
+        path:'/music',
+        bgColor:'#0f0',
+        icon:'icon-z'
+      },
+      {
+        title:'书籍',
+        name:'book',
+        path:'/book',
+        bgColor:'#00f',
+        icon:'icon-shuji'
+      },
+      {
+        title:'图片',
+        name:'photo',
+        path:'/photo',
+        bgColor:'#ff0',
+        icon:'icon-tupian'
+      },
+    ],
+    selectMenu:{}
+    }
+    
+  }
+}
+</script>
+
+<style lang="scss">
+  .content{
+    margin: 1rem 0;
+  }
+</style>
---
+++ vue-cli/vueapp/src/views/book/Book.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Book</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
