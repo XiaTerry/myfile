2314752555d764381e4eae508accfadf1d5dc22d
XiaTerry <1363314380@qq.com>
2018-08-14 23:47:40 +0800

    vue练习movie列表

--- vue-cli/vueapp/src/views/movie/Movie.vue
+++ vue-cli/vueapp/src/views/movie/Movie.vue
@@ -1,11 +1,137 @@
 <template>
-    <h1>Movie</h1>
+    <div>
+        <div v-show="isMovie">
+            <ul>
+                <li class="movie" v-for="movie in movieList" :key="movie.id" @click="movieDetail(movie)">
+                        <div class="movie-img">
+                            <img :src="movie.images.large" alt="">
+                        </div>
+                        <div class="movie-info">
+                            <div class="movie-info-title">{{movie.title}}</div>
+                            <div>观众评 <span class="movie-info-average">{{movie.rating.average}}</span></div>
+                            <div>主演：
+                                <span v-for="item in movie.casts" :key="item.id">{{item.name}}&nbsp;</span>
+                            </div>
+                        </div>
+                    
+                </li>
+            </ul>
+            <div class="end" v-show="isEnd">
+                <h3>数据到底咯！</h3>
+            </div>
+            <div class="loading" v-show="isLoading">
+                <img src="@/assets/img/timg.gif" alt="">
+            </div>
+        </div>
+        <MovieDetail v-show="isDetail" :detail="detail"></MovieDetail>
+    </div>
+    
 </template>
 <script>
+import axios from 'axios';
+import MovieDetail from './MovieDetail.vue';
 export default {
-    
+    components:{
+        MovieDetail,
+    },
+    data () {
+        return {
+           movieList:[], 
+           isLoading:true,
+           isEnd:false,
+           isMovie:true,
+           isDetail:false,
+           detail:{}
+        //    movieDetail:[],
+        }
+    },
+    methods:{
+        getData(){
+        //   let movieUrl = 'https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters';
+        let movieUrl2 = 'https://api.myjson.com/bins/nn8e4';
+        axios.get(movieUrl2).then(res=>{
+            let getList = res.data.subjects.slice(this.movieList.length,this.movieList.length + 5);
+            if(getList.length < 5){
+                this.isEnd = true;
+            }
+            this.movieList = this.movieList.concat(getList);
+            this.isLoading = false;
+        });
+        },
+        movieDetail(movie){
+            this.isMovie = false;
+            this.isDetail = true;
+            this.detail = movie;
+            // this.$emit('movieDetail',isMovie,isDetail)
+            let detailUrl = `https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/subject/${movie.id}`;
+            axios.get(detailUrl).then(res=>{
+                this.detail = res.data;
+                // console.log(res.data);
+            });
+        }
+    },
+    beforeCreate () {
+      let url = 'https://api.myjson.com/bins/uqxn0';
+      axios.get(url).then(res=>{
+      })  
+    },
+    created() {
+        this.$emit('switchTab','movie');
+        this.getData();
+    },
+    mounted(){
+        window.onscroll= ()=>{
+            let scrollTop = document.documentElement.scrollTop;
+            let scrollHeight = document.documentElement.scrollHeight;
+            let clientHeight = document.documentElement.clientHeight;
+            if(scrollHeight = scrollTop + clientHeight && !this.isEnd){
+                this.getData();
+            }
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
-
+    .movie{
+        display: flex;
+        padding: 0.2rem;
+        &-img{
+            
+            flex-grow: 1;
+            width: 0;
+            img{
+                border-radius: 0.1rem;
+                width: 100%;
+            }
+        }
+        &-info{
+            color: #666;
+            flex-grow:3;
+            width: 0;
+            margin-left: 0.2rem;
+            border-bottom: 0.005rem solid #333;
+            &-average{
+                color:#faaf00;
+                font-size: 0.34rem;
+                font-weight: 700;
+            }
+            &-title{
+                color: #000;
+                font-size: 0.4rem;
+                font-weight: 600;
+            }
+        }
+    }
+    .loading{
+        text-align: center;
+        position: fixed;
+        bottom: 1rem;
+        width: 100%;
+        img{
+            width: 3rem;
+        }
+    }
+    .end{
+        text-align: center;
+    }
 </style>
---
+++ vue-cli/vueapp/src/views/movie/MovieDetail.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <div class="moviedetail">
+        <div class="moviedetail-img">
+            <img :src="detail.images.large" alt="">
+        </div>
+        <div class="moviedetail-info">
+            <h3 class="moviedetail-info-title">{{detail.original_title}}</h3>
+            <div>{{detail.aka[1]}}</div>
+            <div>评分：<span class="moviedetail-info-average">{{detail.rating.average}}</span></div>
+            <div>{{detail.genres[2]}},{{detail.genres[1]}}</div>
+            <div>{{detail.countries[0]}}</div> 
+            <div>{{detail.year}}</div>
+        </div>
+    </div>
+    <div class="detai-content">{{detail.summary}}</div>
+</div>
+    
+</template>
+<script>
+export default {
+    props:["detail"],
+}
+</script>
+
+<style lang="scss" scoped>
+    .moviedetail{
+        display: flex;
+        padding: 0.2rem;
+        &-img{
+            flex-grow: 2;
+            width: 0;
+            img{
+                border-radius: 0.1rem;
+                width: 100%;
+            }
+        }
+        &-info{
+            color: #666;
+            flex-grow:2;
+            width: 0;
+            margin-left: 0.2rem;
+            &-average{
+                color:#faaf00;
+                font-size: 0.34rem;
+                font-weight: 700;
+            }
+            &-title{
+                color: #000;
+                font-size: 0.4rem;
+                font-weight: 600;
+            }
+        }
+    }
+    .detai-content{
+        padding: 0 0.2rem;
+    }
+</style>
