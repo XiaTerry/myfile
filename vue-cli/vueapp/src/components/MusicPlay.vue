f7892d74cef97c0a8f08777909b97ca770e311be
XiaTerry <1363314380@qq.com>
2018-08-16 23:20:24 +0800

    vue音乐播放

--- vue-cli/vueapp/src/components/MusicPlay.vue
+++ vue-cli/vueapp/src/components/MusicPlay.vue
@@ -15,28 +15,34 @@
                         <i class="iconfont icon-play" @click="play"  v-show="!isPlay"></i>
                         <i class="iconfont icon-stop" @click="pause"  v-show="isPlay"></i>
                         <i class="iconfont icon-yduixiayiqu" @click="next"></i>
+                        <i class="iconfont icon-yuanxunhuanbofang" @click="loop" v-show="isLoop"></i>
+                        <i class="iconfont icon-xunhuanbofang" @click="notLoop" v-show="isloop"></i>
                     </div>
-                    <span @click="toggleList=!toggleList" class="album-info-control-menu">歌单</span>
+                    <span @click="toggleList=!toggleList" class="album-info-control-menu">
+                        <button style="background:#009688;border-radius:0.15rem;outline:none">歌单</button>  
+                    </span>
                 </div>
             </div>
         </div>
         <!-- 音乐菜单 -->
         <transition name="slide">
             <ul class="music-list" v-show="toggleList">
+                
                 <li :class="['music-list-item',nowIndex == index?'selected':'']" v-for="(music,index) in musicList" :key="index" @click="selectedMusic(music,index)">
                 <span  class="music-list-item-title">{{music.title}}</span>&nbsp;-&nbsp; 
                 <span class="music-list-item-author">{{music.author}}</span>
                 </li>
             </ul>
         </transition>
-        <div class="song">
-            <!-- {{lrc}} -->
-            {{g}}
-        </div>
+           <ul class="song" ref="songList" @click="toggleList=!toggleList">
+               <li v-for="(list,index) in lrcList" :key="index" :class="[lrcIndex ==index?'selected': '']">
+                   {{list.lrc}}
+               </li>
+           </ul>
         
         <!-- 播放控制 -->
-        <div class="audio" :style="'height:'+height+';width:100%'">
-            <audio id="player" ref="musicAudio"  @play="isPlay = true" @pause="isPlay = false" :src="musicSrc" autoplay controls class="audio-ctrl"></audio>
+        <div class="audio">
+            <audio id="player" ref="musicAudio"  @play="isPlay = true" @pause="isPlay = false" :src="musicSrc" autoplay controls  class="audio-ctrl"></audio>
         </div>
        
     </div>
@@ -45,111 +51,81 @@
 import '@/assets/font/iconfont.css';
 import axios from 'axios';
 export default {
-    props:["musicList","musicc"],
+    props:["musicList"],
     data () {
         return {
             nowIndex:-1,
-            toggleList:true,
+            toggleList:false,
             albumImg:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534324016192&di=5f9745e5123e451bbaa5262ea964f437&imgtype=0&src=http%3A%2F%2Fpic.orsoon.com%2F2016%2F0503%2F20160503031530930.png',
             albumTitle:'',
             albumAuthor:'',
             musicSrc:'',
-            g:'',
-            gd:{
-                up:null,
-                down:null,
-            },
-            p:null,
-            lrcObj:null,
-            height:'auto',
             isPlay:false,
-            lrc:'',
-            Lrc:['刚好遇见你','就是爱你','广东爱情故事','我要你','最美的期待','追光者']
+            lrcList:[],
+            // t:null,
+            lrcIndex:-1,
+            isLoop:true,
+            isloop:false,
         }
     },
-    created(){
-       
-    },
-   
     methods:{
         selectedMusic(music,index){
             this.nowIndex = index;
-            this.Lrc.forEach(item=>{
-            if(music.title == item){
-                axios.get(`/lrc/${item}.lrc`).then(res=>{
-                this.lrc = res.data;
-                console.log(typeof(res.data));
-                
-            })
-            }
-        })
         },
         next(){
             this.nowIndex--;
             if(this.nowIndex ==-1){
                 this.nowIndex= this.musicList.length -1 ;
             }
-            this.getLrc();
         },
         prev(){
             this.nowIndex++;
             if(this.nowIndex == this.musicList.length){
                 this.nowIndex = 0;
             }  
-           console.log('buffered=',this.$refs.musicAudio.played);
-            console.log('duration=',this.$refs.musicAudio.duration);
-            console.log((this.$refs.musicAudio.currentTime)/1000);
         },
         play(){
             this.$refs.musicAudio.play();
-             console.log(this.$refs.musicAudio.currentTime);
         },
         pause(){
             this.$refs.musicAudio.pause();
         },
-        getStartDate(){
-            this.$refs.musicAudio.getStartDate();
+        loop(){
+            console.log(this.$refs.musicAudio.loop);
+            this.$refs.musicAudio.loop =true;
+            this.isloop = true;
+            this.isLoop = false;
         },
-        getLrc(){
-            var lyrics = this.lrc.split("\n");
-            var lrcObj = {};
-            for(var i=1;i<this.lrc.length;i++){
-                var lyric = decodeURIComponent(lyrics[i]);
-                // console.log(lyric);
-                var timeReg = /\[\d*:\d*((\.|\:)\d*)*\]/g;
-                var timeRegExpArr = lyric.match(timeReg);
-                if(!timeRegExpArr)continue;
-                var clause = lyric.replace(timeReg,'');
-                for(var k = 0,h = timeRegExpArr.length;k < h;k++) {
-                    var t = timeRegExpArr[k];
-                    var min = Number(String(t.match(/\[\d*/i)).slice(1)),
-                    sec = Number(String(t.match(/\:\d*/i)).slice(1));
-                    var time = min * 60 + sec;
-                    lrcObj[time] = clause;
+        notLoop(){
+            console.log(this.$refs.musicAudio.loop);
+            this.$refs.musicAudio.loop = false;
+            this.isloop = false;
+            this.isLoop = true;
+        },
+        getLrc(song){
+            // console.log(lrc);
+            let line = song.split('\n');
+            // console.log(line);
+            line.forEach(elem => {
+                let time = elem.match(/\[\d{2}:\d{2}.\d{2}\]/);
+                // console.log(time);
+                if(time !=null){
+                    let lrc = elem.split(time)[1];
+                    // console.log(lrc);
+                    // console.log(time[0]);
+                    let timeReg = time[0].match(/(\d{2}):(\d{2}).(\d{2})/);
+                    // console.log(timeReg);
+                    let time2Seconds = parseInt(timeReg[1]) * 60 + parseInt(timeReg[2]) + parseInt(timeReg[3]) / 1000;
+                    // console.log(time2Seconds);
+                    this.lrcList.push({
+                        lrc:lrc,
+                        time:time2Seconds
+                    });
+                    
                 }
-            }
-            return lrcObj;
-        } 
-    },
-    created(){
-        console.log(this);
-        console.log(document.getElementById("#player"));
-        console.log(123);
-        console.log(this.$refs.musicAudio);
-        this.p = document.querySelector("#player");
-        
-        let _this = this;
-        this.lrcObj = this.getLrc();
-        console.log(this.lrcObj);
-        this.p.addEventListener("timeupdate",function(){
-        let obj = _this.lrcObj[Math.floor(this.currentTime)];
-        if(obj!=undefined){
-            _this.g = obj;
-        }
-        });
-        this.height =window.innerHeight+'px'
-        window.onresize  = ()=>{
-        _this.height =window.innerHeight+'px'
+
+
+            });
         }
     },
      watch:{
@@ -159,15 +135,38 @@ export default {
             this.albumAuthor = nowMusic.author;
             this.albumTitle = nowMusic.title;
             this.musicSrc = nowMusic.src;
-            this.Lrc.forEach(item=>{
-            if(nowMusic.title == item){
-                axios.get(`/lrc/${item}.lrc`).then(res=>{
-                this.lrc = res.data;
-            })
-            }
-        })
-
+            this.lrcList = [];
+            this.lrcIndex = -1;
+            axios.get('/'+ nowMusic.lrc).then(res=>{
+                this.getLrc(res.data);
+                // this.lrc = res.data;
+                
+            });
+            //  console.log(this.lrcIndex);
         }
+    },
+     mounted(){
+        let musicAudio =   this.$refs.musicAudio;
+        musicAudio.addEventListener('timeupdate',()=>{
+            let currentTime = musicAudio.currentTime;
+            this.lrcList.forEach((elem,index)=>{
+                if(Math.ceil(elem.time) >= currentTime && Math.floor(elem.time) < currentTime){
+                    this.lrcIndex = index;
+                    // console.log(Math.ceil(elem.time));
+                    this.$refs.songList.scrollTop = this.lrcIndex * 25;
+                    // console.log(this.$refs.songList.scrollTop);
+                }
+            })
+
+        });
+       musicAudio.addEventListener('ended',()=>{
+           this.nowIndex++;
+           if(this.nowIndex == this.musicList.length){
+               this.nowIndex = 0;
+           }
+       });
+        
+     
     },
   
 }
@@ -192,13 +191,24 @@ i:nth-child(3){
 }
 i:nth-child(4){
     top: 1.2rem;
-    margin-left: -1.5rem;
+    margin-left: -1.6rem;
+}
+i:nth-child(5){
+    top: 1.2rem;
+}
+i:nth-child(6){
+    top: 1.2rem;
+    // margin-left: 0.6rem;
 }
 .album{
     display: flex;
     position: fixed;
     text-align: center;
-    position: relative;
+    // position: relative;
+    margin-bottom: 3rem;
+    overflow: hidden;
+    height: 2.3rem;
+    width: 100%;
     color: #fff;
     &-mask{
         position: absolute;
@@ -225,6 +235,7 @@ i:nth-child(4){
             font-size: 0.5rem;
         }
         &-control{
+            // position: relative;
             &-icon{
                 i{
                     font-size: 0.5rem;
@@ -234,10 +245,12 @@ i:nth-child(4){
                 }
             }
             &-menu{
+                // position: absolute;
                 float: right;
-                font-size: 0.3rem;
-                margin-right: 0.8rem;
-                margin-top: 0.1rem;
+                font-size: 0.35rem;
+                margin-right: 0.2rem;
+                color: #000;
+                margin-top: 0.35rem;
             }
         }
         
@@ -292,9 +305,21 @@ i:nth-child(4){
     }
 }
 .song{
-    background: blue;
-    // opacity: 0.3;
+    position: fixed;
+    top: 3.3rem;
+    left: 0;
+    right: 0;
+    bottom: 2.3rem;
+    text-align: center;
+    overflow-y: scroll;
     color: #000;
+    z-index: -1;
+    padding-top: 2rem;
+    font-size: 0.3rem;
+    .selected{
+        color: #299557;
+        font-size:0.4rem;
+    }
 }
 .audio{
     background: #f1f3f4;
--- vue-cli/vueapp/src/views/music/Music.vue
+++ vue-cli/vueapp/src/views/music/Music.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Music</h1>
+   <MusicPlay :musicList="musicList" ></MusicPlay>
 </template>
 
 <script>
+import MusicPlay from '@/components/MusicPlay';
+import axios from 'axios';
 export default {
-    
+    components:{
+        MusicPlay,
+    },
+    data () {
+        return {
+            musicList:[],
+        }
+    },
+    created() {
+        this.$emit('switchTab','music');
+        // console.log('book');
+        let url = 'https://api.myjson.com/bins/xo01o'
+        let musicUrl = '/data/musicdata.json';
+        axios.get(musicUrl).then(res=>{
+            this.musicList = res.data.musicData;
+        });
+        axios.get(url).then(res=>{
+            console.log(res.data.data.song.list);
+        })
+    },
 }
 </script>
 <style lang="scss" scoped>
