d13e9808c2af5b50e406612fbd84b5ceb4bdb1f0
XiaTerry <1363314380@qq.com>
2018-08-17 22:48:59 +0800

    图片

--- vue-cli/vueapp/src/views/photo/Photo.vue
+++ vue-cli/vueapp/src/views/photo/Photo.vue
@@ -1,12 +1,36 @@
 <template>
-    <h1>Photo</h1>
+    <ul class="clearfix">
+        <li class="photo" @click="gotodetail(index)" v-for="(photo,index) in  $store.state.photoList" :key="photo.src">
+           <img :src="photo.src" alt="">
+        </li>
+    </ul>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    
+    created() {
+        this.$emit('switchTab','photo');
+        console.log(this.$route.path);
+        axios.get('/data/photodata.json').then(res=>{
+            this.$store.commit('setPhotoList',res.data.photoData);
+        })
+    }, 
+    methods:{
+        gotodetail(index){
+            this.$router.push(`/photodetail/${index}`);
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
-
+    .clearfix::after{
+        content: '';
+        display:block;
+        clear: both;
+    }
+    .photo{
+        float: left;
+        width: 50%;
+    }
 </style>
